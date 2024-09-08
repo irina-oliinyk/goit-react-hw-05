e4b501fc2de00dd2a1ea4851171c0779f28ffbd6
Iryna Oliinyk <arairishka2@gmail.com>
2024-09-09 01:53:51 +0200

    +lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-// import { useState } from 'react'
-import Navigation from './components/Navigation/Navigation';
 import { Route, Routes } from 'react-router-dom';
-import { Suspense } from 'react';
+import { Suspense, lazy } from 'react';
 import './App.css';
 
-import HomePage from './pages/HomePage/HomePage';
-import MovieDetailsPage from './pages/MovieDetailsPage/MovieDetailsPage';
-import MoviesPage from './pages/MoviesPage/MoviesPage';
-import MovieCast from './components/MovieCast/MovieCast';
-import MovieReviews from './components/MovieReviews/MovieReviews';
-import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('./pages/MoviesPage/MoviesPage'));
+const MovieDetailsPage = lazy(() =>
+  import('./pages/MovieDetailsPage/MovieDetailsPage'),
+);
+const MovieCast = lazy(() => import('./components/MovieCast/MovieCast'));
+const MovieReviews = lazy(() =>
+  import('./components/MovieReviews/MovieReviews'),
+);
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
+const Navigation = lazy(() => import('.//components/Navigation/Navigation'));
 
 function App() {
   return (
